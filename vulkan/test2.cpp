750b39a00756e431773f87ccf75ff9e04dd5940e
tpoisonooo <khj.application@aliyun.com>
2022-03-17 18:44:22 +0800

    update

--- vulkan/test2.cpp
+++ vulkan/test2.cpp
@@ -2,69 +2,66 @@
 #include "Shader.hpp"
 #include "types.h"
 
-std::vector<float> kompute(const std::string& shader, unsigned int m, unsigned int k, unsigned int n) {
+float kompute(const std::string& shader_template, unsigned int m, unsigned int k, unsigned int n) {
+    constexpr int local_size = 32;
+    char shader[2048] = {0};
+    sprintf(shader, shader_template.c_str(), local_size, local_size);
+    fprintf(stdout, "%s\n", shader);
 
-    // 1. Create Kompute Manager with default settings (device 0, first queue and no extensions)
     kp::Manager mgr;
 
     auto dtype = kp::Tensor::TensorDataTypes::eFloat;
 
     // define Input & Output
-    AlignVector matrixA(m * k, 2.f);
-    // AlignVector matrixB(k * n, 2.f);
-    // std::vector<float> matrixC;
-    // matrixC.resize(k * n);
-
-    // std::vector<float> matrixC(m*n, 3.f);
+    AlignVector matrixA(m * k, 1.f);
+    AlignVector matrixB(k * n, 1.f);
+    AlignVector matrixC(m * n);
 
     // Create and initialise Kompute Tensors through manager
-    // auto tensorInA = mgr.tensor({2., 2., 2., 2.});
     auto tensorInA = mgr.tensor(matrixA.data(), matrixA.size(), sizeof(float), dtype);
-    auto tensorInB = mgr.tensor({2., 2., 2., 2.});
-    auto tensorInC = mgr.tensorT<float>({3., 3., 3., 3.});
+    auto tensorInB = mgr.tensor(matrixB.data(), matrixB.size(), sizeof(float), dtype);
+    auto tensorInC = mgr.tensor(matrixC.data(), matrixC.size(), sizeof(float), dtype);
 
     std::vector<std::shared_ptr<kp::Tensor>> params = {tensorInA, tensorInB, tensorInC};
 
     // Create algorithm based on shader (supports buffers & push/spec constants)
-    constexpr int local_size = 2;
-    // kp::Workgroup workgroup({m / local_size, n / local_size});
-    kp::Workgroup workgroup({1, 1, 1});
-
+    kp::Workgroup workgroup({m / local_size, n / local_size, 1});
 
     auto algorithm = mgr.algorithm(params,
-                                   // See documentation shader section for compileSource
                                    compileSource(shader),
                                    workgroup,
-                                   {2.});
+                                   { k * 1.f});
 
-    // Run operation synchronously using sequence
     mgr.sequence()
         ->record<kp::OpTensorSyncDevice>(params)
         ->record<kp::OpAlgoDispatch>(algorithm)
+        ->record<kp::OpTensorSyncLocal>(params)
         ->eval();
 
-    auto sq = mgr.sequence();
-    sq->record<kp::OpTensorSyncLocal>(params)
-        ->eval();
-
-    return tensorInC->vector();
+    float* result = tensorInC->data<float>();
+    constexpr int LEN = 4;
+    for (int i = 0; i < LEN; ++i) {
+        fprintf(stdout, "tensorInC raw %d: %f\n", i, result[i]);
+    }
 }
 
-int main() {
+    // diff = compare_matrices(m, n, cold, ldc, cref, ldc);
+    // if (diff > 0.5f || diff < -0.5f) {
+    //   printf("diff too big !\n");
+    //   exit(-1);
+    // }
 
-    // Define a raw string shader (or use the Kompute tools to compile to SPIRV / C++ header
-    // files). This shader shows some of the main components including constants, buffers, etc
-    std::string shader = (R"(
+int main() {
+    std::string shader_template = (R"(
         #version 450
 
-        layout (local_size_x = 2, local_size_y = 2) in;
+        layout (local_size_x = %d, local_size_y = %d) in;
 
         layout (set = 0, binding = 0) readonly buffer buf_in_tensor_1 { float in_tensor_1[]; };
         layout (set = 0, binding = 1) readonly buffer buf_in_tensor_2 { float in_tensor_2[]; };
         layout (set = 0, binding = 2) writeonly buffer buf_out_tensor { float out_tensor[]; };
 
         layout (constant_id = 0) const float tensor_size_f = 0;
-
 
         void main()
         {
@@ -81,9 +78,6 @@ int main() {
     )");
 
     // Run the function declared above with our raw string shader
-    auto result = kompute(shader, 2, 2, 2);
-    constexpr int LEN = 4;
-    for (int i = 0; i < LEN; ++i) {
-        fprintf(stdout, "%d: %f\n", i, result[i]);
-    }
+    constexpr int SIZE = 256;
+    auto gflops = kompute(shader_template, SIZE, SIZE, SIZE);
 }
--- vulkan/test_MMult.cpp
+++ vulkan/test_MMult.cpp
@@ -4,14 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-// CUDA runtime
-#include "helper.h"
-#include <cublas_v2.h>
-#include <cuda_runtime.h>
-
 void REF_MMult(int, int, int, float *, int, float *, int, float *, int);
-void MY_MMult(cublasHandle_t, int, int, int, float *, int, float *, int,
-              float *, int);
+float MY_MMult(int, int, int, float *, float *, float *);
 void copy_matrix(int, int, float *, int, float *, int);
 void random_matrix(int, int, float *, int);
 float compare_matrices(int, int, float *, int, float *, int);
@@ -19,19 +13,6 @@ float compare_matrices(int, int, float *, int, float *, int);
 double dclock();
 
 int main() {
-  // print gpu info
-  cudaDeviceProp deviceProp;
-  int devID = 0;
-  checkCudaErrors(cudaSetDevice(devID));
-  auto error = cudaGetDeviceProperties(&deviceProp, devID);
-  if (error != cudaSuccess) {
-    printf("cudaGetDeviceProperties returned error code %d, line(%d)\n", error,
-           __LINE__);
-    exit(EXIT_FAILURE);
-  }
-  printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devID,
-         deviceProp.name, deviceProp.major, deviceProp.minor);
-
   int p, m, n, k, lda, ldb, ldc, rep;
 
   double dtime, dtime_best, gflops, diff;
@@ -39,18 +20,6 @@ int main() {
   float *a, *b, *c, *cref, *cold;
 
   printf("MY_MMult = [\n");
-
-  cublasHandle_t handle;
-  checkCudaErrors(cublasCreate(&handle));
-  // checkCudaErrors(cublasSetMathMode(handle, CUBLAS_TENSOR_OP_MATH));
-
-  /* Time the "optimized" implementation */
-  cudaEvent_t start, stop;
-  // Allocate CUDA events that we'll use for timing
-  checkCudaErrors(cudaEventCreate(&start));
-  checkCudaErrors(cudaEventCreate(&stop));
-
-  // printf( "create Handle\n");
 
   for (p = PFIRST; p <= PLAST; p += PINC) {
     m = (M == -1 ? p : M);
@@ -82,51 +51,17 @@ int main() {
     memset(cold, 0, mem_size_C);
     memset(cref, 0, mem_size_C);
 
-    /* Init device matrix*/
-    float *d_A, *d_B, *d_C;
-    checkCudaErrors(cudaMalloc((void **)&d_A, mem_size_A));
-    checkCudaErrors(cudaMalloc((void **)&d_B, mem_size_B));
-    checkCudaErrors(cudaMemcpy(d_A, a, mem_size_A, cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMemcpy(d_B, b, mem_size_B, cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMalloc((void **)&d_C, mem_size_C));
-
     /* Run the reference implementation so the answers can be compared */
     // printf( "init\n");
 
     REF_MMult(m, n, k, a, lda, b, ldb, cref, ldc);
     // printf( "benchmark\n");
-
-    // Record the start event
-    checkCudaErrors(cudaEventRecord(start, NULL));
-
+    float avg_gflops = 0.0;
     for (rep = 0; rep < NREPEATS; rep++) {
       /* Time your implementation */
-      MY_MMult(handle, m, n, k, d_A, lda, d_B, ldb, d_C, ldc);
+      avg_gflops = MY_MMult(m, n, k, a, b, c);
     }
 
-    // printf( "mymmult\n");
-
-    // Record the stop event
-    checkCudaErrors(cudaEventRecord(stop, NULL));
-    // Wait for the stop event to complete
-    checkCudaErrors(cudaEventSynchronize(stop));
-    float msecTotal = 0.0f;
-    checkCudaErrors(cudaEventElapsedTime(&msecTotal, start, stop));
-
-    // Compute and print the performance
-    float msecPerMatrixMul = msecTotal / NREPEATS;
-    double flopsPerMatrixMul = 2.0 * m * k * n;
-    double gflops =
-        (flopsPerMatrixMul * 1.0e-9f) / (msecPerMatrixMul / 1000.0f);
-
-    // copy result from device to host
-    checkCudaErrors(cudaMemcpy(cold, d_C, mem_size_C, cudaMemcpyDeviceToHost));
-
-    diff = compare_matrices(m, n, cold, ldc, cref, ldc);
-    if (diff > 0.5f || diff < -0.5f) {
-      printf("diff too big !\n");
-      exit(-1);
-    }
     printf("%d %.2f %le \n", p, gflops, diff);
 
     free(a);
@@ -134,14 +69,7 @@ int main() {
     free(c);
     free(cold);
     free(cref);
-
-    checkCudaErrors(cudaFree(d_A));
-    checkCudaErrors(cudaFree(d_B));
-    checkCudaErrors(cudaFree(d_C));
   }
-
-  // Destroy the handle
-  checkCudaErrors(cublasDestroy(handle));
 
   printf("];\n");
   return 0;
